b041c19690afb7ad6b98690a5dd26ae8dbad962b
gitgudscrub <Ryan.mavin@gmail.com>
2026-09-20 13:37:19 +0100

    Defer Night Info marker return until sync

--- src/client/java/com/sharktower/bloodonthesharktower/client/gui/NightInfoReminderScreen.java
+++ src/client/java/com/sharktower/bloodonthesharktower/client/gui/NightInfoReminderScreen.java
@@ -66,8 +66,6 @@ public final class NightInfoReminderScreen extends Screen {
                                         "add_role_reminder",
                                         seat + "|" + marker.role().getId() + "|" + marker.text()
                                 );
-                                GrimoireReturnState.suppressNextReveal();
-                                this.minecraft.gui.setScreen(new AssignRolesScreen());
                             })
                     .bounds(x, y + row * 25, buttonW, 20).build());
         }
