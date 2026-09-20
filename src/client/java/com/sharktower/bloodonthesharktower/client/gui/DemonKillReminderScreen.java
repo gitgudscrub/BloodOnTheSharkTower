9bdec9ba489d69ace475efd6f8f0e3381b828297
gitgudscrub <Ryan.mavin@gmail.com>
2026-09-20 13:31:44 +0100

    Return Demon kill reminders immediately to Grim

--- src/client/java/com/sharktower/bloodonthesharktower/client/gui/DemonKillReminderScreen.java
+++ src/client/java/com/sharktower/bloodonthesharktower/client/gui/DemonKillReminderScreen.java
@@ -51,6 +51,8 @@ public final class DemonKillReminderScreen extends Screen {
                     "add_role_reminder",
                     seat + "|" + demon.getId() + "|Kill"
             );
+            GrimoireReturnState.suppressNextReveal();
+            minecraft.gui.setScreen(new AssignRolesScreen());
             return;
         }
 
@@ -81,6 +83,8 @@ public final class DemonKillReminderScreen extends Screen {
                                         "add_role_reminder",
                                         seat + "|" + demon.getId() + "|Kill"
                                 );
+                                GrimoireReturnState.suppressNextReveal();
+                                this.minecraft.gui.setScreen(new AssignRolesScreen());
                             })
                     .bounds(x, y + row * 25, buttonW, 20).build());
         }
